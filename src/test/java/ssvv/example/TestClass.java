95559d28f0f9d9a6423076e04e9041dd18b4af19
paulhideg <83585235+paulhideg@users.noreply.github.com>
2024-04-12 08:50:31 +0300

    added lab4_ic

---
+++ src/test/java/ssvv/example/TestClass.java
@@ -0,0 +1,224 @@
+package ssvv.example;
+
+import domain.Tema;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeAll;
+import repository.NotaXMLRepo;
+import repository.StudentXMLRepo;
+import repository.TemaXMLRepo;
+import service.Service;
+import validation.NotaValidator;
+import validation.StudentValidator;
+import validation.TemaValidator;
+import validation.ValidationException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TestClass {
+
+    public static Service service;
+
+    @BeforeAll
+    public static void setup() {
+        StudentValidator studentValidator = new StudentValidator();
+        TemaValidator temaValidator = new TemaValidator();
+        String filenameStudent = "fisiere/Studenti.xml";
+        String filenameTema = "fisiere/Teme.xml";
+        String filenameNota = "fisiere/Note.xml";
+
+        StudentXMLRepo studentXMLRepository = new StudentXMLRepo(filenameStudent);
+        TemaXMLRepo temaXMLRepository = new TemaXMLRepo(filenameTema);
+        NotaValidator notaValidator = new NotaValidator(studentXMLRepository, temaXMLRepository);
+        NotaXMLRepo notaXMLRepository = new NotaXMLRepo(filenameNota);
+        TestClass.service = new Service(studentXMLRepository, studentValidator, temaXMLRepository, temaValidator, notaXMLRepository, notaValidator);
+    }
+
+    // White box testing
+    @Test
+    public void addTema_ValidData_CreateSuccessfully() {
+        String nrTema = "100";
+        String descriere = "desc";
+        int deadline = 12;
+        int primire = 1;
+        Tema tema = new Tema(nrTema, descriere, deadline, primire);
+        try {
+            service.addTema(tema);
+            assert(true);
+        } catch (ValidationException exception) {
+            assert(false);
+        }
+    }
+
+    @Test
+    public void addTema_Invalid_nrTema_duplicate_ThrowsError() {
+        String nrTema = "100";
+        String descriere = "desc";
+        int deadline = 12;
+        int primire = 1;
+        Tema tema = new Tema(nrTema, descriere, deadline, primire);
+        try {
+            Tema response = service.addTema(tema);
+            assert(tema == response);
+        } catch (ValidationException exception) {
+            assert(true);
+        }
+    }
+
+    @Test
+    public void addTema_Invalid_nrTema_emptyString_ThrowsError() {
+        String nrTema = "";
+        String descriere = "desc";
+        int deadline = 12;
+        int primire = 2;
+        Tema tema = new Tema(nrTema, descriere, deadline, primire);
+        try {
+            service.addTema(tema);
+            assert(false);
+        } catch (ValidationException exception) {
+            assert(true);
+        }
+    }
+
+    @Test
+    public void addTema_Invalid_nrTema_null_ThrowsError() {
+        String nrTema = null;
+        String descriere = "desc";
+        int deadline = 12;
+        int primire = 2;
+        Tema tema = new Tema(nrTema, descriere, deadline, primire);
+        try {
+            service.addTema(tema);
+            assert(false);
+        } catch (ValidationException exception) {
+            assert(true);
+        }
+    }
+
+    @Test
+    public void addTema_Invalid_descriere_emptyString_ThrowsError() {
+
+        String nrTema = "101";
+        String descriere = "";
+        int deadline = 12;
+        int primire = 2;
+
+        Tema tema = new Tema(nrTema, descriere, deadline, primire );
+
+        try{
+            service.addTema(tema);
+            assert(false);
+
+        }catch (ValidationException ve){
+            assert(true);
+
+        }
+
+    }
+
+//    @Test
+//    public void addTema_Invalid_descriere_null_ThrowsError() {
+//
+//        String nrTema = "101";
+//        String descriere = null;
+//        int deadline = 12;
+//        int primire = 2;
+//
+//        Tema tema = new Tema(nrTema, descriere, deadline, primire );
+//
+//        try{
+//            service.addTema(tema);
+//            assert(false);
+//
+//        }catch (ValidationException ve){
+//            System.out.println("Validation Exception: " + ve.getMessage());
+//            assert(true);
+//
+//        }
+//
+//    }
+
+
+    @Test
+    public void addTema_Invalid_deadline_smallerThan1_ThrowsError() {
+
+        String nrTema = "102";
+        String descriere = "desc";
+        int deadline = 0;
+        int primire = 11;
+
+        Tema tema = new Tema(nrTema, descriere, deadline, primire );
+
+        try{
+            service.addTema(tema);
+            assert(false);
+
+        }catch (ValidationException ve){
+            assert(true);
+
+        }
+
+    }
+
+    @Test
+    public void addTema_Invalid_deadline_greaterThan14_ThrowsError() {
+
+        String nrTema = "102";
+        String descriere = "desc";
+        int deadline = 15;
+        int primire = 11;
+
+        Tema tema = new Tema(nrTema, descriere, deadline, primire );
+
+        try{
+            service.addTema(tema);
+            assert(false);
+
+        }catch (ValidationException ve){
+            assert(true);
+
+        }
+
+    }
+
+    @Test
+    public void addTema_Invalid_primire_smallerThan1_ThrowsError() {
+
+        String nrTema = "103";
+        String descriere = "desc";
+        int deadline = 12;
+        int primire = 0;
+
+        Tema tema = new Tema(nrTema, descriere, deadline, primire );
+
+        try{
+            service.addTema(tema);
+            assert(false);
+
+        }catch (ValidationException ve){
+            assert(true);
+
+        }
+
+    }
+
+    @Test
+    public void addTema_Invalid_primire_greaterThan14_ThrowsError() {
+
+        String nrTema = "103";
+        String descriere = "desc";
+        int deadline = 12;
+        int primire = 15;
+
+        Tema tema = new Tema(nrTema, descriere, deadline, primire );
+
+        try{
+            service.addTema(tema);
+            assert(false);
+
+        }catch (ValidationException ve){
+            assert(true);
+
+        }
+
+    }
+}
